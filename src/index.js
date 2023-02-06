ecb573716492397a764495bdb015f432bcd69151
codingvaleria <codingvaleria@gmail.com>
2023-02-06 14:28:26 +0300

    Add a task to form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  // As a user, I should be able to type a task into the input field.
+  // As a user, I should be able to click some form of a submit button.
+  // As a user, I expect to see the task string that I provided appear in the DOM after the submit button has been activated.
+
+  function addToDo(todo) {
+    let task = document.createElement("li");
+    task.textContent = todo;
+    document.querySelector("#tasks").appendChild(task);
+  }
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addToDo(e.target.new_task_description.value);
+    form.reset();
+  });
+
+  //
 });
